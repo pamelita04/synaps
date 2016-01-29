1839ff0f3829f43a4f7ee9e0de1dd92a944e458f
luselen70 <luselen70@gmail.com>
2016-01-29 14:36:16 -0400

    funciones javascript 1er sprint

---
+++ js/js.js
@@ -0,0 +1,131 @@
+function precionado1(){ 
+        document.getElementById("pagina1").style.display = "none";
+    
+}
+
+function precionado2(){ 
+        document.getElementById("pagina2").style.display = "none";
+    
+}
+function precionado3(){ 
+        document.getElementById("pagina3").style.display = "none";
+    
+}
+function precionado4(){ 
+        document.getElementById("pagina4").style.display = "none";
+    
+}
+function precionado5(){ 
+        document.getElementById("pagina5").style.display = "none";
+    
+}
+/*-------------------------------------------------*/
+function click1(){ 
+        document.getElementById("pagina1").style.display = "inline";
+document.getElementById("pagina2").style.display = "none";
+document.getElementById("pagina3").style.display = "none";
+document.getElementById("pagina4").style.display = "none";
+document.getElementById("pagina5").style.display = "none";
+document.getElementById("pagina6").style.display = "none";
+document.getElementById("pagina7").style.display = "none";
+document.getElementById("pagina8").style.display = "none";
+    
+}
+
+function click2(){ 
+        document.getElementById("pagina2").style.display = "inline";
+document.getElementById("pagina1").style.display = "none";
+document.getElementById("pagina3").style.display = "none";
+ document.getElementById("pagina4").style.display = "none"; 
+document.getElementById("pagina5").style.display = "none";
+document.getElementById("pagina6").style.display = "none";
+document.getElementById("pagina7").style.display = "none";
+document.getElementById("pagina8").style.display = "none";
+}
+function click3(){ 
+        document.getElementById("pagina3").style.display = "inline";
+
+document.getElementById("pagina1").style.display = "none";
+document.getElementById("pagina2").style.display = "none";
+ document.getElementById("pagina4").style.display = "none"; 
+document.getElementById("pagina5").style.display = "none";
+document.getElementById("pagina6").style.display = "none";
+document.getElementById("pagina7").style.display = "none";
+document.getElementById("pagina8").style.display = "none";
+    
+}
+function click4(){ 
+        document.getElementById("pagina4").style.display = "inline";
+    
+document.getElementById("pagina1").style.display = "none";
+document.getElementById("pagina2").style.display = "none";
+document.getElementById("pagina3").style.display = "none";
+document.getElementById("pagina5").style.display = "none";
+document.getElementById("pagina6").style.display = "none";
+document.getElementById("pagina7").style.display = "none";
+document.getElementById("pagina8").style.display = "none";
+    
+}
+function click5(){ 
+        document.getElementById("pagina5").style.display = "inline";
+    
+document.getElementById("pagina1").style.display = "none";
+    
+document.getElementById("pagina2").style.display = "none";
+document.getElementById("pagina3").style.display = "none";
+document.getElementById("pagina4").style.display = "none";
+document.getElementById("pagina6").style.display = "none";
+document.getElementById("pagina7").style.display = "none";
+document.getElementById("pagina8").style.display = "none";
+    
+}
+
+function click6(){ 
+        document.getElementById("pagina6").style.display = "inline";
+    
+document.getElementById("pagina1").style.display = "none";
+    
+document.getElementById("pagina2").style.display = "none";
+document.getElementById("pagina3").style.display = "none";
+document.getElementById("pagina4").style.display = "none";
+document.getElementById("pagina5").style.display = "none";
+document.getElementById("pagina7").style.display = "none";
+document.getElementById("pagina8").style.display = "none";
+    
+}
+
+function click7(){ 
+        document.getElementById("pagina7").style.display = "inline";
+    
+document.getElementById("pagina1").style.display = "none";
+document.getElementById("pagina2").style.display = "none";
+document.getElementById("pagina3").style.display = "none";
+document.getElementById("pagina4").style.display = "none";
+document.getElementById("pagina5").style.display = "none";
+document.getElementById("pagina6").style.display = "none";
+document.getElementById("pagina8").style.display = "none";
+    
+}
+
+
+function click8(){ 
+        document.getElementById("pagina8").style.display = "inline";
+    
+document.getElementById("pagina1").style.display = "none";
+document.getElementById("pagina2").style.display = "none";
+document.getElementById("pagina3").style.display = "none";
+document.getElementById("pagina4").style.display = "none";
+document.getElementById("pagina5").style.display = "none";
+document.getElementById("pagina6").style.display = "none";
+document.getElementById("pagina7").style.display = "none";
+    
+}
+
+/*function generarCodigoLibro(){
+    var campNombre =  document.getElementById("nombreAutor").value.toUpperCase().replace(",","").replace(".","").split(" ",2)[0];
+    
+    var campAnio = document.getElementById("anioEdicion").value.substring(2,4);
+    
+    document.getElementById("codigoLibro").value ="["+ campNombre + campAnio + "]";
+             
+}*/
